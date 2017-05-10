1fb392dd335d92629f65928f5195b3a0f2abd310
edisonfang <edisonfang@edisonfangdeMacBook-Air.local>
2017-05-10 15:38:32 +0800

    是非ＡＰＰ

--- select/ViewController.swift
+++ select/ViewController.swift
@@ -9,6 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var star: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var yes: UIImageView!
+    @IBOutlet weak var no: UIImageView!
+    
+    
+    
+    @IBAction func buttom(_ sender: Any) {
+        if star.text == "FF"{                       //記得要打.text
+            if age.text == "jkl"{
+                yes.isHidden = false
+                no.isHidden = true
+            }
+            else {
+                yes.isHidden = true
+                no.isHidden = false}
+            
+            }
+        else {
+            yes.isHidden = true
+            no.isHidden = false
+    }
+}       //記得括號數目
 
     override func viewDidLoad() {
         super.viewDidLoad()
